25d31fac58c427deb161c234c8f90b39a747cd7e
Freddy Wicaksono <59552422+freddywicaksono@users.noreply.github.com>
2020-12-23 20:06:26 +0700

    Add files via upload

---
+++ Connection.vb
@@ -0,0 +1,78 @@
+﻿Imports System.Data.SQLite
+Public Class Connection
+    Private _version As Integer = 3
+    Private _filepath As String
+    Private _new As Boolean = False
+    Private _pwd As String
+    Private _found As Boolean
+    Private _connected As Boolean = False
+
+    Public Property DataSource()
+        Get
+            Return _filepath
+        End Get
+        Set(ByVal value)
+            _filepath = value
+        End Set
+    End Property
+
+    Public Property Password()
+        Get
+            Return _pwd
+        End Get
+        Set(ByVal value)
+            _pwd = value
+        End Set
+    End Property
+
+    Public Property NewFile()
+        Get
+            Return _new
+        End Get
+        Set(ByVal value)
+            _new = value
+        End Set
+    End Property
+
+    Public Property RecordFound() As Boolean
+        Get
+            Return _found
+        End Get
+        Set(ByVal value As Boolean)
+            _found = value
+        End Set
+    End Property
+
+    ''' <summary>
+    ''' Proses menyambungkan koneksi (SQL Server Express with Integrated Security = True without Username or Password)
+    ''' </summary>
+    Public Sub Connect()
+        Try
+            conn.Close()
+            conn = New SQLiteConnection("Data Source=" & _filepath & ";Version=" & _version.ToString & ";New=" & _new & ";")
+
+            conn.Open()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString())
+        Finally
+            If (conn.State = True) Then
+                _connected = True
+            Else
+                _connected = False
+            End If
+        End Try
+    End Sub
+
+    ''' <summary>
+    ''' Proses memutuskan koneksi
+    ''' </summary>
+    Public Sub Disconnect()
+        If (conn.State = True) Then
+            conn.Close()
+            conn.Dispose()
+            _connected = False
+        End If
+    End Sub
+
+End Class
---
+++ MyMod.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Data.SQLite
+Module MyMod
+    Public mycommand As New System.Data.SQLite.SQLiteCommand
+    Public myadapter As New System.Data.SQLite.SQLiteDataAdapter
+    Public mydata As New DataTable
+    Public DR As System.Data.SQLite.SQLiteDataReader
+    Public SQL As String
+    Public conn As New System.Data.SQLite.SQLiteConnection
+    Public cn As New Connection
+
+    'Tabel Fakultas
+    '-------------------------------
+    Public fakultas_baru As Boolean
+    Public ofakultas As New Fakultas
+    '--------------------------------
+
+    Public Sub DBConnect()
+        cn.DataSource = "C:\Users\UMC-LN-03\Documents\data freddy\coba.db"
+        cn.Connect()
+    End Sub
+
+    Public Sub DBDisconnect()
+        cn.Disconnect()
+    End Sub
+End Module
